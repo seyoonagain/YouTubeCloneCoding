e7a49c6cf3eed57d8d9cec74c620b9b5a835d791
Seyoon Jeong <seyoonagain@gmail.com>
2024-07-19 05:12:00 +0900

    deleted unused variable

--- src/Context/YoutubeApiContext.jsx
+++ src/Context/YoutubeApiContext.jsx
@@ -1,12 +1,11 @@
 import { createContext, useContext } from 'react';
 import Youtube from '../API/youtube';
-import FakeYoutubeClient from '../API/fakeYoutubeClient';
+
 import YoutubeClient from '../API/youtubeClient';
 
 export const YoutubeApiContext = createContext();
 
 export default function YoutubeApiProvider({ children }) {
-    // const client = new FakeYoutubeClient();
     const client = new YoutubeClient();
     const youtube = new Youtube(client);
     return (
